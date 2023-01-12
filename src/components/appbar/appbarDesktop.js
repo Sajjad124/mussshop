6a498432a4f294635e2d1d48ea0af795ac50d25e
Muhammad Umair <181370124@gift.edu.pk>
2023-01-12 23:12:32 +0500

    Update appbarDesktop.js

--- src/components/appbar/appbarDesktop.js
+++ src/components/appbar/appbarDesktop.js
@@ -1,27 +1,37 @@
 import { Search } from "@mui/icons-material";
 import { ListItemButton, ListItemIcon, ListItemText } from "@mui/material";
 import React from "react";
-import { AppBarContainer, AppBarHeader, MyList } from "../../styles/appbar";
+import {
+  ActionIconsContainerDesktop,
+  ActionIconsContainerMobile,
+  AppBarContainer,
+  AppBarHeader,
+  MyList,
+} from "../../styles/appbar";
 import Actions from "./actions";
 
-function AppBarDesktop({matches}) {
+function AppBarDesktop({ matches }) {
+  const Component = matches
+    ? ActionIconsContainerMobile
+    : ActionIconsContainerDesktop;
   return (
-    <AppBarContainer>
-      <AppBarHeader>Muss Shop</AppBarHeader>
-      <MyList type="row">
-        <ListItemText primary="Home" />
-        <ListItemText primary="Categories" />
-        <ListItemText primary="Products" />
-        <ListItemText primary="Contact" />
-        {/* <ListItemText primary="About Us" /> */}
-        <ListItemButton>
-          <ListItemIcon>
-            <Search />
-          </ListItemIcon>
-        </ListItemButton>
-      </MyList>
-      <Actions matches={matches} />
-    </AppBarContainer>
+    <Component>
+      <AppBarContainer>
+        <AppBarHeader>Muss Shop</AppBarHeader>
+        <MyList type="row">
+          <ListItemText primary="Home" />
+          <ListItemText primary="Categories" />
+          <ListItemText primary="Products" />
+          <ListItemText primary="Contact" />
+          <ListItemButton>
+            <ListItemIcon>
+              <Search />
+            </ListItemIcon>
+          </ListItemButton>
+        </MyList>
+        <Actions matches={matches} />
+      </AppBarContainer>
+    </Component>
 
     /*
     AppBar Container
