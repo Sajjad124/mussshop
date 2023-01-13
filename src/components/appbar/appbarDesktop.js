3810c8dbf30b33ab9919aaa105893893571d945f
Muhammad Umair <181370124@gift.edu.pk>
2023-01-13 23:41:28 +0500

    Update appbarDesktop.js

--- src/components/appbar/appbarDesktop.js
+++ src/components/appbar/appbarDesktop.js
@@ -11,27 +11,27 @@ import {
 import Actions from "./actions";
 
 function AppBarDesktop({ matches }) {
-  const Component = matches
-    ? ActionIconsContainerMobile
-    : ActionIconsContainerDesktop;
+  // const Component = matches
+  //   ? ActionIconsContainerMobile
+  //   : ActionIconsContainerDesktop;
   return (
-    <Component>
-      <AppBarContainer>
-        <AppBarHeader>Muss Shop</AppBarHeader>
-        <MyList type="row">
-          <ListItemText primary="Home" />
-          <ListItemText primary="Categories" />
-          <ListItemText primary="Products" />
-          <ListItemText primary="Contact" />
-          <ListItemButton>
-            <ListItemIcon>
-              <Search />
-            </ListItemIcon>
-          </ListItemButton>
-        </MyList>
-        <Actions matches={matches} />
-      </AppBarContainer>
-    </Component>
+    // <Component>
+    <AppBarContainer>
+      <AppBarHeader>Muss Shop</AppBarHeader>
+      <MyList type="row">
+        <ListItemText primary="Home" />
+        <ListItemText primary="Categories" />
+        <ListItemText primary="Products" />
+        <ListItemText primary="Contact" />
+        <ListItemButton>
+          <ListItemIcon>
+            <Search />
+          </ListItemIcon>
+        </ListItemButton>
+      </MyList>
+      <Actions matches={matches} />
+    </AppBarContainer>
+    // </Component>
 
     /*
     AppBar Container
